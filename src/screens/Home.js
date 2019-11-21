dd050564c29340bbfc83002a5981bad5e27a5431
Svetlana Siarska <siarska@gmail.com>
2019-11-21 15:51:30 +0100

    design HomeScreen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,8 +1,78 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, StyleSheet, Dimensions} from 'react-native';
+import {RectButton} from 'react-native-gesture-handler';
 
 class HomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewHeight: 0,
+    };
+    this.onLayoutView = this.onLayoutView.bind(this);
+  }
+
+  onLayoutView(event) {
+    const viewHeight = event.nativeEvent.layout.height;
+    this.setState({viewHeight});
+  }
+
   render() {
+    const {width} = Dimensions.get('window');
+
+    const {viewHeight} = this.state;
+    const borderRadius = Math.floor(width / 4);
+    const w = borderRadius * 2;
+    const top = Math.floor(viewHeight / 2 - borderRadius);
+    const left = Math.floor(width / 2 - borderRadius);
+    const marginTopBox = borderRadius + 50;
+
+    return (
+      <View style={{flex: 1, flexDirection: 'column'}}onLayout={this.onLayoutView}>
+        <View style={{flex: 1, backgroundColor: 'blue'}} />
+        <View style={{flex: 1, backgroundColor: 'red', flexDirection: 'row'}}>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'white',
+              borderRadius: 10,
+              margin: 20,
+              marginTop: marginTopBox,
+            }}>
+            <Text> BOX1</Text>
+          </View>
+          <View
+            style={{
+              flex: 1,
+              backgroundColor: 'white',
+              borderRadius: 10,
+              margin: 20,
+              marginTop: marginTopBox,
+            }}>
+            <Text> BOX2</Text>
+          </View>
+        </View>
+        <View
+          style={{
+            ...styles.floatingButtonBig,
+            width: w,
+            height: w,
+            borderRadius,
+            top,
+            left,
+            flexDirection: 'column',
+}}>
+          <View style={{...styles.item, flex: 2}}>
+            <Text>test</Text>
+          </View>
+          <View style={{...styles.item, flex: 1, justifyContent: 'flex-start'}}>
+            <Text>test</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+
+  render1() {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
         <Text>Home!</Text>
@@ -16,3 +86,36 @@ class HomeScreen extends React.Component {
 }
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  floatingButtonBig: {
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center',
+    left: 0,
+    top: 0,
+    backgroundColor: 'white',
+  },
+  floatingMenu: {
+    position: 'absolute',
+    width: '100%',
+    height: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    left: 0,
+    bottom: 0,
+    backgroundColor: '#3780ff',
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  item: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderWidth: 1,
+    borderColor: 'black',
+  },
+});
